46977cc5bc1917590947b957d5767bedff5f590e
nikneem <ekeilholz+git@outlook.com>
2025-03-11 10:18:01 +0100

    Removing comments

--- src/Votr.Api/Program.cs
+++ src/Votr.Api/Program.cs
@@ -12,11 +12,7 @@ builder.AddServiceDefaults()
     .WithTableStorage()
     .AddAzureTableClient("votes");
 
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
